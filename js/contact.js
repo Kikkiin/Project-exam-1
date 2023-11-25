32cc4275cf2d0594e0e4d499323c08a7cef0309e
Kikkiin <125892429+Kikkiin@users.noreply.github.com>
2023-11-25 18:23:21 +0100

    change contact form

--- js/contact.js
+++ js/contact.js
@@ -3,22 +3,29 @@ function validateForm() {
     var email = document.forms["contactForm"]["email"].value;
     var subject = document.forms["contactForm"]["subject"].value;
     var message = document.forms["contactForm"]["message"].value;
+    var isValid = true;
+
+    document.getElementById("nameError").textContent = '';
+    document.getElementById("emailError").textContent = '';
+    document.getElementById("subjectError").textContent = '';
+    document.getElementById("messageError").textContent = '';
 
     if (name.length < 5) {
-        alert("Name must be at least 5 characters long.");
-        return false;
+        document.getElementById("nameError").textContent = "Name must be at least 5 characters long.";
+        isValid = false;
     }
     if (!email.includes('@') || !email.includes('.')) {
-        alert("Please enter a valid email address.");
-        return false;
+        document.getElementById("emailError").textContent = "Please enter a valid email address.";
+        isValid = false;
     }
     if (subject.length < 15) {
-        alert("Subject must be at least 15 characters long.");
-        return false;
+        document.getElementById("subjectError").textContent = "Subject must be at least 15 characters long.";
+        isValid = false;
     }
     if (message.length < 25) {
-        alert("Message content must be at least 25 characters long.");
-        return false;
+        document.getElementById("messageError").textContent = "Message content must be at least 25 characters long.";
+        isValid = false;
     }
-    return true;
+
+    return isValid;
 }
