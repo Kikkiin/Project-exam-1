e265dcba43c497c3200080a9041f114191c5c944
Kikkiin <125892429+Kikkiin@users.noreply.github.com>
2023-11-27 23:20:36 +0100

    testing import/export

---
+++ js/fetchPosts.js
@@ -0,0 +1,39 @@
+export async function fetchPostsByCategory(apiUrl) {
+    try {
+      const response = await fetch(apiUrl);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const posts = await response.json();
+      const postsContainer = document.querySelector('.blogposts');
+      postsContainer.innerHTML = ''; // Rensker tidligere innlegg
+  
+      posts.forEach(post => {
+        const postElement = document.createElement('div');
+        postElement.classList.add('post');
+
+        const postLink = document.createElement('a');
+        postLink.href = `postDetails.html?id=${post.id}`;
+        postLink.classList.add('post-link');
+
+        postLink.innerHTML = `
+        <div class="post-header">
+          <h2>${post.title.rendered}</h2>
+        </div>
+        <div class="post-image">
+          ${post.better_featured_image ? `<img src="${post.better_featured_image.source_url}" alt="${post.title.rendered}">` : ''}
+        </div>
+        <div class="post-excerpt">
+          <p>${post.excerpt.rendered}</p>
+        </div>
+        `;
+
+        postElement.appendChild(postLink);
+        postsContainer.appendChild(postElement);
+      });
+
+    } catch (error) {
+      console.error('Error fetching posts:', error);
+      showError(error.message);
+    }
+}
